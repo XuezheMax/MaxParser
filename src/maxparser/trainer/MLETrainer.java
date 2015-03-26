1bc79191666e87e9da0b7a2356f4179d2b40f0c3
xuezhem <xuezhem@cs.cmu.edu>
2015-03-25 23:11:08 -0400

    warm start for MLETrainer

--- src/maxparser/trainer/MLETrainer.java
+++ src/maxparser/trainer/MLETrainer.java
@@ -1,6 +1,8 @@
 package maxparser.trainer;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 import maxparser.DependencyInstance;
@@ -30,107 +32,127 @@ public class MLETrainer extends Trainer{
 		int threadNum = model.threadNum();
 		Manager[] managers = createManagers(manager, trainfile, threadNum, numTrainInst, model);
 		
-		double old_obj = 1e+37;
 		double cost = model.cost();
-		int converge = 0;
+		
+		String trainforest = model.trainforest();
 		int nsize = model.featureSize();
 		
-		SimpleLBFGS lbfgs = new SimpleLBFGS(nsize);
+		double bestAcc = 0.0;
+		double bestC = 0.0;
+		PrintWriter logWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(logfile)));
 		
-		//init threads
-		String trainforest = model.trainforest();
-		
-		for(int itr = 0; itr < maxIter; ++itr){
-			System.out.flush();
-			long clock = System.currentTimeMillis() / 1000;
-			
-			GradientCollectThread[] threads = new GradientCollectThread[threadNum];
-			int unit = (numTrainInst % threadNum == 0) ? numTrainInst / threadNum : numTrainInst / threadNum + 1;
-			for(int i = 0; i < threadNum; ++i){
-				int start = unit * i;
-				int end = start + unit;
-				if(end > numTrainInst){
-					end = numTrainInst;
+		for(double c = ((devfile != null && cost > 0.005) ? 0.005 : cost); c <= cost; c += 0.005){
+			double old_obj = 1e+37;
+			int converge = 0;
+			SimpleLBFGS lbfgs = new SimpleLBFGS(nsize);
+			System.out.println(String.format("current cost:  %.3f", c));
+			for(int itr = 0; itr < maxIter; ++itr){
+				System.out.flush();
+				long clock = System.currentTimeMillis() / 1000;
+				
+				GradientCollectThread[] threads = new GradientCollectThread[threadNum];
+				int unit = (numTrainInst % threadNum == 0) ? numTrainInst / threadNum : numTrainInst / threadNum + 1;
+				for(int i = 0; i < threadNum; ++i){
+					int start = unit * i;
+					int end = start + unit;
+					if(end > numTrainInst){
+						end = numTrainInst;
+					}
+					String[] tokens = trainforest.split("\\.");
+					String forestfile = tokens[0] + i + "." + tokens[1];
+					threads[i] = new GradientCollectThread(start, end, managers[i], decoder, forestfile, model);
 				}
-				String[] tokens = trainforest.split("\\.");
-				String forestfile = tokens[0] + i + "." + tokens[1];
-				threads[i] = new GradientCollectThread(start, end, managers[i], decoder, forestfile, model);
-			}
-			
-			for(int i = 0; i < threadNum; ++i){
-				threads[i].start();
-			}
-			for(int i = 0; i < threadNum; ++i){
+				
+				for(int i = 0; i < threadNum; ++i){
+					threads[i].start();
+				}
+				for(int i = 0; i < threadNum; ++i){
+					try {
+						threads[i].join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						System.exit(0);
+					}
+				}
+				
+				for(int i = 1; i < threadNum; ++i){
+					threads[0].obj += threads[i].obj;
+				}
+				
+				for(int i = 1; i < threadNum; ++i){
+					for(int j = 0; j < nsize; ++j){
+						threads[0].gradient[j] += threads[i].gradient[j];
+					}
+				}
+				
+				threads[0].obj *= c;
+				
+				for(int j = 0; j < nsize; ++j){
+					threads[0].obj += model.paramAt(j) * model.paramAt(j) / 2.0;//(2.0 * c);
+					threads[0].gradient[j] *= c;
+					threads[0].gradient[j] += model.paramAt(j);// / c;
+				}
+				
+				double diff = itr == 0 ? 1.0 : Math.abs(old_obj - threads[0].obj) / old_obj;
+				System.out.println("iter=" + itr + " obj=" + String.format("%.8f", threads[0].obj) + " diff=" + String.format("%.8f", diff) + " time=" + (System.currentTimeMillis() /1000 - clock) + "s.");
+				old_obj = threads[0].obj;
+				
+				if(diff < eta){
+					converge++;
+				}
+				else{
+					converge = 0;
+				}
+				
+				if(itr > maxIter || converge == 3){
+					break;
+				}
+				
 				try {
-					threads[i].join();
-				} catch (InterruptedException e) {
+					if(model.update(lbfgs, threads[0].obj, threads[0].gradient) == 0){
+						break;
+					}
+				} catch (ExceptionWithIflag e) {
 					e.printStackTrace();
 					System.exit(0);
 				}
 			}
 			
-			for(int i = 1; i < threadNum; ++i){
-				threads[0].obj += threads[i].obj;
-			}
+			System.out.print("Saving Model...");
+			long clock = System.currentTimeMillis() / 1000;
+			saveModel(model, modelfile);
+			System.out.println("Done. Took: " + (System.currentTimeMillis() / 1000 - clock) + "s.");
 			
-			for(int i = 1; i < threadNum; ++i){
-				for(int j = 0; j < nsize; ++j){
-					threads[0].gradient[j] += threads[i].gradient[j];
+			if(devfile != null){
+				//calc current accuracy
+				logWriter.println(String.format("Cost:  %.3f", c));
+				double acc = evalCurrentAcc(manager, decoder, model, devfile, logWriter, numDevInst);
+				logWriter.println("------------------------------------------");
+				logWriter.flush();
+				if(bestAcc < acc){
+					bestAcc = acc;
+					saveModel(model, modelfile);
+					bestC = c;
 				}
 			}
-			
-			for(int j = 0; j < nsize; ++j){
-				threads[0].obj += model.paramAt(j) * model.paramAt(j) / (2.0 * cost);
-				threads[0].gradient[j] += model.paramAt(j) / cost;
-			}
-			
-			double diff = itr == 0 ? 1.0 : Math.abs(old_obj - threads[0].obj) / old_obj;
-			System.out.println("iter=" + itr + " obj=" + String.format("%.8f", threads[0].obj) + " diff=" + String.format("%.8f", diff) + " time=" + (System.currentTimeMillis() /1000 - clock) + "s.");
-			old_obj = threads[0].obj;
-			
-			if(diff < eta){
-				converge++;
-			}
-			else{
-				converge = 0;
-			}
-			
-			if(itr > maxIter || converge == 3){
-				break;
-			}
-			
-			try {
-				if(model.update(lbfgs, threads[0].obj, threads[0].gradient) == 0){
-					break;
-				}
-			} catch (ExceptionWithIflag e) {
-				e.printStackTrace();
-				System.exit(0);
-			}
 		}
+		logWriter.close();
+		System.out.println("Final Dev Accuracy: " + bestAcc + "% (Cost: " + bestC + ")");
 		
-		System.out.print("Saving Model...");
-		long clock = System.currentTimeMillis() / 1000;
-		saveModel(model, modelfile);
-		System.out.println("Done. Took: " + (System.currentTimeMillis() / 1000 - clock) + "s.");
-		
-		if(devfile != null){
-			evalCurrentAcc(manager, decoder, model, devfile, numDevInst);
-		}
 	}
 	
-	protected void evalCurrentAcc(Manager manager, Decoder decoder, ParserModel model, String devfile, int numDevInst) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
-		ObjectReader in = new ObjectReader(model.devforest());
+	protected double evalCurrentAcc(Manager manager, Decoder decoder, ParserModel tempModel, String devfile, PrintWriter logWriter, int numDevInst) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
+		ObjectReader in = new ObjectReader(tempModel.devforest());
 		String tempfile = "tmp/result.tmp";
 		
-		DependencyWriter tempWriter = DependencyWriter.createDependencyWriter(model.getWriter());
+		DependencyWriter tempWriter = DependencyWriter.createDependencyWriter(tempModel.getWriter());
 		tempWriter.startWriting(tempfile);
 		
 		for(int j = 0; j < numDevInst; ++j){
 			DependencyInstance inst = null;
-			inst = manager.readInstance(in, model);
+			inst = manager.readInstance(in, tempModel);
 			
-			Pair<FeatureVector, String>[] d = decoder.decode(manager, inst, 1, model);
+			Pair<FeatureVector, String>[] d = decoder.decode(manager, inst, 1, tempModel);
 			String[] res = d[0].second.split(" ");
 			int[] heads = new int[inst.length()];
 			String[] types = new String[inst.length()];
@@ -139,7 +161,7 @@ public class MLETrainer extends Trainer{
 			for(int k = 1; k < inst.length(); k++){
 				String[] trip = res[k - 1].split("[\\|:]");
 				heads[k] = Integer.parseInt(trip[0]);
-				types[k] = model.getType(Integer.parseInt(trip[2]));
+				types[k] = tempModel.getType(Integer.parseInt(trip[2]));
 			}
 			
 			tempWriter.write(inst, heads, types);
@@ -150,10 +172,7 @@ public class MLETrainer extends Trainer{
 		tempWriter.close();
 		
 		//evaluate current acc
-		PrintWriter printer = new PrintWriter(System.out);
-		printer.println("Performance:");
-		maxparser.Evaluator.evaluate(devfile, tempfile, printer, model);
-		printer.flush();
+		return maxparser.Evaluator.evaluate(devfile, tempfile, logWriter, tempModel);
 	}
 	
 	private Manager[] createManagers(Manager manager, String trainfile, int threadNum, int numTrainInst, ParserModel model) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
