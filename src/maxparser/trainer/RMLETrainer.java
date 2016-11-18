7b1e60940bccfb2f97aa4e3a3aa888387cb43e55
xuezhem <xuezhem@cs.cmu.edu>
2016-11-18 14:43:13 -0500

    cost

--- src/maxparser/trainer/RMLETrainer.java
+++ src/maxparser/trainer/RMLETrainer.java
@@ -45,7 +45,7 @@ public class RMLETrainer extends Trainer {
         double bestTau = 0.0;
         PrintWriter logWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(logfile)));
 
-        for (double c = ((devfile != null && cost > 0.030) ? 0.020 : cost); c <= cost; c += 0.005) {
+        for (double c = ((devfile != null && cost > 0.030) ? 0.030 : cost); c <= cost; c += 0.005) {
             for (double t = ((devfile != null && tau > 0.1) ? 0.25 : tau); t <= tau + 0.05; t += 0.01) {
                 double old_obj = 1e+37;
                 int converge = 0;
